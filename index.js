3af239c43e5dc32d488093d76796beab759d4965
Mohammad Kaif Shaikh <mokaifshaikh0@gmail.com>
2025-08-23 02:35:48 +0530

    new feature added

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const fullName = "Mohmmad Kaif Shaikh";
 const number= 9569063030;
 console.log(fullName);
 console.log(number);
+let age =18;
+function checkAge(age){
+    if(age>=18){
+        console.log("eligible");
+    }
+}
+checkAge(age);
